9d2d46b54fa3566db0058fbcb0882cf79c800e5c
qk27015_zitte <tk5760a@gmail.com>
2019-01-25 10:05:34 +0900

    ReplaceMagicNumberWithSymbolicConstant after

---
+++ 001_ReplaceMagicNumberWithSymbolicConstant_after/src/RobotCommand.java
@@ -0,0 +1,12 @@
+
+public class RobotCommand {
+	private final String _name;
+	
+	public RobotCommand(String name) {
+		_name = name;
+	}
+	
+	public String toString() {
+		return "[ RobotCommand: " + _name + " ]";
+	}
+}
